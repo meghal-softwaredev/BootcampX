e7df26947f6802437d8cf273c6c790398ab2f011
Meghal <meghal.shah210@gmail.com>
2021-10-11 17:57:21 -0400

    BootcampX Queries II - JOIN

---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,8 @@
+-- Get the total amount of time that all students from a specific cohort have spent on all assignments.
+SELECT SUM(duration) AS total_duration
+FROM cohorts
+JOIN students
+ON cohorts.id = cohort_id
+JOIN assignment_submissions 
+ON students.id = student_id
+WHERE cohorts.name = 'FEB12'; 
