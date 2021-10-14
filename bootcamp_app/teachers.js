e33e4d65869e7ab938c9dbbdc967ed96ddff9ee2
Meghal <meghal.shah210@gmail.com>
2021-10-13 23:34:40 -0400

    Node Postgres bootcampx database

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,19 +8,22 @@ const pool = new Pool({
 });
 
 
-const queryString = `
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-FROM students
+const queryString = `SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests
+ON teachers.id = teacher_id
+JOIN students
+ON student_id = students.id
 JOIN cohorts
 ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 2};
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teachers.name;
 `;
 
 pool.query(queryString)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.teacher}`);
     })
   })
   .catch(err => {
